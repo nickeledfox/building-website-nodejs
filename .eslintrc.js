1f2de0b3ab80bab0fff000da05c5a10490b53383
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-01-21 16:58:17 -0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "airbnb-base",
+  quotes: [2, "single"],
+  globals: {
+    SwaggerEditor: false,
+  },
+  env: {
+    browser: true,
+  },
+  rules: {
+    "linebreak-style": 0,
+  },
+};
